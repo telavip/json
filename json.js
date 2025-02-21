e0f2c095aad32cc9e0198df8454fbad0f438d4d6
telavip <megateca@outlook.com>
2025-02-21 15:50:00 -0300

    Create json.js

---
+++ json.js
@@ -0,0 +1,12 @@
+[
+  {
+    "titulo": "Filme Exemplo",
+    "descricao": "Um ótimo filme de ação",
+    "imagem": "https://exemplo.com/capa.jpg"
+  },
+  {
+    "titulo": "Série Exemplo",
+    "descricao": "Uma série incrível",
+    "imagem": "https://exemplo.com/serie.jpg"
+  }
+]

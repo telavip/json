be0f0391cee980beaa707f8ec369bff2dfb53b4f
telavip <megateca@outlook.com>
2025-02-22 10:11:50 -0300

    Update and rename dadex.js to dadexx.js

---
+++ dadexx.js
@@ -0,0 +1,67 @@
+  window.seriesData = {
+    "281652": {
+        "name": "Moana: Fãs de Carteirinha",
+        "genres": [
+            "Reality"
+        ],
+        "overview": "Fãs do mundo inteiro homenageiam Moana. Esta série apresenta os melhores vídeos de fãs.",
+        "first_air_date": "2024-12-08",
+        "episode_run_time": "Desconhecido",
+        "vote_average": "10.0",
+        "number_of_seasons": 1,
+        "number_of_episodes": 10,
+        "created_by": [],
+        "production_companies": [],
+        "cast": [],
+        "backdrop_path": "https://image.tmdb.org/t/p/original/rV2gNifLRh6fsTsZXsxZmaVqL5H.jpg",
+        "poster_path": "https://image.tmdb.org/t/p/original/2jh03i0BxLDnz5hi8ZgDPMKkYub.jpg",
+        "seasons": {
+            "1": {
+                "name": "Temporada 1",
+                "poster_path": "https://image.tmdb.org/t/p/original/2jh03i0BxLDnz5hi8ZgDPMKkYub.jpg",
+                "episodes": {
+                    "1": {
+                        "name": "Episódio 1",
+                        "still_path": "https://image.tmdb.org/t/p/original/8VUJS24uD6J5B7i6A0aUBN830D9.jpg"
+                    },
+                    "2": {
+                        "name": "Episódio 2",
+                        "still_path": "https://image.tmdb.org/t/p/original/aH0Xd5dIRyIMeuGe748xNeQuB3S.jpg"
+                    },
+                    "3": {
+                        "name": "Episódio 3",
+                        "still_path": "https://image.tmdb.org/t/p/original/fHIxnaERNqd1mkzKMTEkMADEUDD.jpg"
+                    },
+                    "4": {
+                        "name": "Episódio 4",
+                        "still_path": "https://image.tmdb.org/t/p/original/g9vO7g4wHHR2LGmpTqosLjN4fiB.jpg"
+                    },
+                    "5": {
+                        "name": "Episódio 5",
+                        "still_path": "https://image.tmdb.org/t/p/original/nLD01z5Yj5g0mbEa2hvZoTBWn4M.jpg"
+                    },
+                    "6": {
+                        "name": "Episódio 6",
+                        "still_path": "https://image.tmdb.org/t/p/original/pyHvte8Aoqo0Q7xvd46UCJ9osrJ.jpg"
+                    },
+                    "7": {
+                        "name": "Episódio 7",
+                        "still_path": "https://image.tmdb.org/t/p/original/o4EGYsNftgMlkKv28XtxitjcIfR.jpg"
+                    },
+                    "8": {
+                        "name": "Episódio 8",
+                        "still_path": "https://image.tmdb.org/t/p/original/iODDpsKIobwWXZTXVPvyeMCckyg.jpg"
+                    },
+                    "9": {
+                        "name": "Episódio 9",
+                        "still_path": "https://image.tmdb.org/t/p/original/8wHcmy3EUGMpCXFvy2GAjbDFRM0.jpg"
+                    },
+                    "10": {
+                        "name": "Episódio 10",
+                        "still_path": "https://image.tmdb.org/t/p/original/blJiQFUmcqcbiY6CO4U8EVVT4b3.jpg"
+                    }
+                }
+            }
+        }
+    },
+  };

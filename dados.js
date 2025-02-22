2d4fccf93f587e0c0e6d9ac835e39c560de00af9
telavip <megateca@outlook.com>
2025-02-22 10:04:27 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -425,5 +425,212 @@ window.seriesData = {
       }
     }
   },  
-   
+      "80623": {
+        "name": "Baki - O Campeão",
+        "genres": [
+            "Animação",
+            "Action & Adventure",
+            "Crime",
+            "Drama"
+        ],
+        "overview": "Enquanto o campeão de artes marciais Baki Hanma treina duro para superar seu lendário pai, cinco condenados à pena de morte partem para Tóquio para acabar com ele.",
+        "first_air_date": "2018-06-26",
+        "episode_run_time": 24,
+        "vote_average": "8.1",
+        "number_of_seasons": 2,
+        "number_of_episodes": 39,
+        "created_by": [],
+        "production_companies": [
+            "TMS Entertainment"
+        ],
+        "cast": [
+            "Nobunaga Shimazaki",
+            "Kenjiro Tsuda",
+            "Takehito Koyasu",
+            "Issei Futamata",
+            "Banjo Ginga"
+        ],
+        "backdrop_path": "https://image.tmdb.org/t/p/original/qbxoEhlxNcyr7GjG2SXTShWn32m.jpg",
+        "poster_path": "https://image.tmdb.org/t/p/original/hTlJIKptJedaho7N42xCdX0dJyY.jpg",
+        "seasons": {
+            "0": {
+                "name": "Especiais",
+                "poster_path": null,
+                "episodes": {
+                    "1": {
+                        "name": "Episódio 1",
+                        "still_path": "https://image.tmdb.org/t/p/original/D0eCNRZKFUSqM22F0HfDDHp9vr.jpg"
+                    }
+                }
+            },
+            "1": {
+                "name": "Parte 1 e 2",
+                "poster_path": "https://image.tmdb.org/t/p/original/rXbmuaZXH6oR5ni6m7AN6LnGFgg.jpg",
+                "episodes": {
+                    "1": {
+                        "name": "Sincronicidade",
+                        "still_path": "https://image.tmdb.org/t/p/original/uFYdD7AxlEf5OuGyvHLoa1UaIMx.jpg"
+                    },
+                    "2": {
+                        "name": "Artes marciais do submundo",
+                        "still_path": "https://image.tmdb.org/t/p/original/dUFkV1kpyB7IM74vNHG1AwKjHtN.jpg"
+                    },
+                    "3": {
+                        "name": "Eles finalmente chegaram!",
+                        "still_path": "https://image.tmdb.org/t/p/original/uRillQqO7CuaKVs4ROGtG1x3fK8.jpg"
+                    },
+                    "4": {
+                        "name": "Começa a batalha",
+                        "still_path": "https://image.tmdb.org/t/p/original/tjkY1weiJfyvuz81b5OonwoTdNA.jpg"
+                    },
+                    "5": {
+                        "name": "Quer mais?",
+                        "still_path": "https://image.tmdb.org/t/p/original/R9xvNrEsX2uckv7eSnu5Vrxb5.jpg"
+                    },
+                    "6": {
+                        "name": "O relatório do policial Katahira",
+                        "still_path": "https://image.tmdb.org/t/p/original/wt13SSg2ogIqXwZSRJM0ds0ODJ.jpg"
+                    },
+                    "7": {
+                        "name": "Um time incrível",
+                        "still_path": "https://image.tmdb.org/t/p/original/eiJYoc1sln6P9Xf0fNWeul1pZes.jpg"
+                    },
+                    "8": {
+                        "name": "Uma luta de verdade",
+                        "still_path": "https://image.tmdb.org/t/p/original/mlZ3rF7h7bugd0UhMqKqpvN6R2U.jpg"
+                    },
+                    "9": {
+                        "name": "Shin Shin Kai em fúria",
+                        "still_path": "https://image.tmdb.org/t/p/original/uFizeI9GiqNakuRmbT84IBY5HSU.jpg"
+                    },
+                    "10": {
+                        "name": "Duelo aéreo",
+                        "still_path": "https://image.tmdb.org/t/p/original/8fxOpv6uGtg8yK4KbV0ChYp370O.jpg"
+                    },
+                    "11": {
+                        "name": "O matador de tigres",
+                        "still_path": "https://image.tmdb.org/t/p/original/5yLS5jJn5hMiQ1v4JX6wvZ46BLW.jpg"
+                    },
+                    "12": {
+                        "name": "Doce",
+                        "still_path": "https://image.tmdb.org/t/p/original/lXSJzrewh7MEAsEqFrg7ChxgrNe.jpg"
+                    },
+                    "13": {
+                        "name": "Sr. Oliva",
+                        "still_path": "https://image.tmdb.org/t/p/original/wWtvvbW1wo1RHwTL7mRjfvwnsR1.jpg"
+                    },
+                    "14": {
+                        "name": "Liberdade recusada",
+                        "still_path": "https://image.tmdb.org/t/p/original/j4wGL2ST2xRN6ZGWo0z1gB0GQDP.jpg"
+                    },
+                    "15": {
+                        "name": "Montanha de músculos",
+                        "still_path": "https://image.tmdb.org/t/p/original/rwS27SpPnirNoBVcCGwcXQPrG6S.jpg"
+                    },
+                    "16": {
+                        "name": "Corta-corda",
+                        "still_path": "https://image.tmdb.org/t/p/original/nOwm3NiTcLVN2PadFGRCIOTH45X.jpg"
+                    },
+                    "17": {
+                        "name": "Pai!",
+                        "still_path": "https://image.tmdb.org/t/p/original/4L1dZx7izEK4fJI2e1evVjwshxh.jpg"
+                    },
+                    "18": {
+                        "name": "Obrigado",
+                        "still_path": "https://image.tmdb.org/t/p/original/4wByu0B9r7TD5d5bb4us5waethS.jpg"
+                    },
+                    "19": {
+                        "name": "Admite a derrota?",
+                        "still_path": "https://image.tmdb.org/t/p/original/z9A7QkGr1cHmOePMgXSFbNfUsi4.jpg"
+                    },
+                    "20": {
+                        "name": "Golpe chicoteante",
+                        "still_path": "https://image.tmdb.org/t/p/original/807uYl8KsrDVHPAl7ikyPgav9Bc.jpg"
+                    },
+                    "21": {
+                        "name": "Punição",
+                        "still_path": "https://image.tmdb.org/t/p/original/ugKCv7gV5YPdlzP94dXAXjU5Xog.jpg"
+                    },
+                    "22": {
+                        "name": "Confronto entre alfas",
+                        "still_path": "https://image.tmdb.org/t/p/original/digqiX9YSm1mpiHTMNzLcoVGA3y.jpg"
+                    },
+                    "23": {
+                        "name": "Ataque de verdade",
+                        "still_path": "https://image.tmdb.org/t/p/original/8CH6qFVKMImUgIhhHnAhPI7Dfha.jpg"
+                    },
+                    "24": {
+                        "name": "Derrota",
+                        "still_path": "https://image.tmdb.org/t/p/original/figfJ6q15eZSGdJDraP6F3KjioT.jpg"
+                    },
+                    "25": {
+                        "name": "Deus e o diabo",
+                        "still_path": "https://image.tmdb.org/t/p/original/rWp9aXWI3mKcWYlcM8cffYsHLtC.jpg"
+                    },
+                    "26": {
+                        "name": "O Grande Torneio Raitai",
+                        "still_path": "https://image.tmdb.org/t/p/original/3699oIPV4zvjNiEd1GMPQjz0Xst.jpg"
+                    }
+                }
+            },
+            "2": {
+                "name": "A Grande Saga do Torneio Raitai",
+                "poster_path": "https://image.tmdb.org/t/p/original/taarM0OCOQW1heyUaOttIwKbclu.jpg",
+                "episodes": {
+                    "1": {
+                        "name": "Que comece o torneio",
+                        "still_path": "https://image.tmdb.org/t/p/original/uDK2JjN1aEAEHSnR5CvD0MK4WjC.jpg"
+                    },
+                    "2": {
+                        "name": "Reviravolta",
+                        "still_path": "https://image.tmdb.org/t/p/original/q3mkdo9z4lubIAaJl8OiTVJ9Mke.jpg"
+                    },
+                    "3": {
+                        "name": "Recuperado!",
+                        "still_path": "https://image.tmdb.org/t/p/original/34RmY27b875qEj30DCncttP4nN4.jpg"
+                    },
+                    "4": {
+                        "name": "Time formado!",
+                        "still_path": "https://image.tmdb.org/t/p/original/2rREONhrIQ9muLKtawpaTGpV84d.jpg"
+                    },
+                    "5": {
+                        "name": "Mão no bolso",
+                        "still_path": "https://image.tmdb.org/t/p/original/8Vo4cmPa9fa0HcgfnOz4clX16Zo.jpg"
+                    },
+                    "6": {
+                        "name": "Excelente!",
+                        "still_path": "https://image.tmdb.org/t/p/original/xDAsazFaUqfRsOdsWWP8wddkaCI.jpg"
+                    },
+                    "7": {
+                        "name": "Imperador do mar",
+                        "still_path": "https://image.tmdb.org/t/p/original/aebpliGZ3hgFkAXPJ0G7ssIXvPL.jpg"
+                    },
+                    "8": {
+                        "name": "Os mais fortes",
+                        "still_path": "https://image.tmdb.org/t/p/original/AdxeW1UD1DCOZ7cBPtuMXWpAAmg.jpg"
+                    },
+                    "9": {
+                        "name": "Lutador x Mestre",
+                        "still_path": "https://image.tmdb.org/t/p/original/lB6ZkaRa10unknMepIW1Y9mhrhA.jpg"
+                    },
+                    "10": {
+                        "name": "Firme na luta",
+                        "still_path": "https://image.tmdb.org/t/p/original/x61NlMTjKIyxt9Wje10i73C5HVS.jpg"
+                    },
+                    "11": {
+                        "name": "Despertar",
+                        "still_path": "https://image.tmdb.org/t/p/original/8efvJiqwOFP5XYdTLRe0EOSmqDr.jpg"
+                    },
+                    "12": {
+                        "name": "Chegou a hora",
+                        "still_path": "https://image.tmdb.org/t/p/original/ruCF5486yiMVvA84Zje5CwCNd2u.jpg"
+                    },
+                    "13": {
+                        "name": "Vingança",
+                        "still_path": "https://image.tmdb.org/t/p/original/uBEb3EE58jaFJ0HXw4QFsxPrKa6.jpg"
+                    }
+                }
+            }
+        }
+    },
 };

4e6bf301825db4cf1aa65558ca70ad1ed1fabba9
telavip <megateca@outlook.com>
2025-02-23 16:34:28 -0300

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,22 @@
+const movies = [
+{
+    "id": 259172,
+    "slug": "a-caverna-encantada",
+    "title": "A Caverna Encantada",
+    "genre_ids": [
+        10766,
+        10751,
+        10759,
+        18
+    ],
+    "first_air_date": "2024-07-29",
+    "popularity": 928.486,
+    "vote_average": 8,
+    "poster_path": "https://image.tmdb.org/t/p/w200/5GhlwbZ8ogQUf2Xuq8tE2lX7gk7.jpg",
+    "networks": [
+        128
+    ],
+    "certification": "L",
+    "episode_count": 154
+},
+];

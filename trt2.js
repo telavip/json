492fd80557599f9c79f5abf0bf4369f6e5b82d65
telavip <megateca@outlook.com>
2025-02-23 17:27:56 -0300

    Update and rename trt.js to trt2.js

---
+++ trt2.js
@@ -0,0 +1,43 @@
+const movies = [
+
+
+{
+  "id": 950396,
+  "slug": "entre-montanhas",
+  "title": "Entre Montanhas",
+  "genre_ids": [
+    10749,
+    878,
+    53
+  ],
+  "first_air_date": "2025-02-13",
+  "popularity": 1495.874,
+  "vote_average": "7.8",
+  "poster_path": "https://image.tmdb.org/t/p/w200/7zL6RxMPmpPSdU5OTdVC4dtiKcr.jpg",
+  "networks": [
+    350
+  ],
+  "certification": "14",
+  "runtime": 127
+},
+
+{
+  "id": 762509,
+  "slug": "mufasa-o-rei-leo",
+  "title": "Mufasa: O Rei Leão",
+  "genre_ids": [
+    12,
+    10751,
+    16
+  ],
+  "first_air_date": "2024-12-18",
+  "popularity": 1207.142,
+  "vote_average": "7.4",
+  "poster_path": "https://image.tmdb.org/t/p/w200/iMVuv6Gz5fj7vZ51IjRF3AiW87y.jpg",
+  "networks": [],
+  "certification": "10",
+  "runtime": 118
+}
+
+
+  ];

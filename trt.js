0d6934d23bd00623b8d2d2b177b050706229fe84
telavip <megateca@outlook.com>
2025-02-23 17:19:52 -0300

    Create trt.js

---
+++ trt.js
@@ -0,0 +1,20 @@
+const movies = [
+  {
+  "id": 950396,
+  "title": "Entre Montanhas",
+  "genre_ids": [
+    10749,
+    878,
+    53
+  ],
+  "release_date": "2025-02-13",
+  "popularity": 1495.874,
+  "vote_average": "7.8",
+  "poster_path": "https://image.tmdb.org/t/p/w200/7zL6RxMPmpPSdU5OTdVC4dtiKcr.jpg",
+  "runtime": 127,
+  "networks": [
+    350
+  ],
+  "certification": "14"
+}
+  ];

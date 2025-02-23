00dcff2a4dd4714c5e8b783dd8dccce81e75a18a
telavip <megateca@outlook.com>
2025-02-23 17:05:01 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,19 @@
+const movies = [
+{
+    "id": 54054,
+    "title": "Como Agarrar Meu Ex-Namorado",
+    "genre_ids": [
+        28,
+        35,
+        80
+    ],
+    "release_date": "2012-01-26",
+    "popularity": 4.502,
+    "vote_average": "5.6",
+    "poster_path": "https://image.tmdb.org/t/p/w200/au8CiS0UEO23hPMwXbjPIvwvGcK.jpg",
+    "runtime": 91,
+    "networks": [],
+    "certification": "12",
+    "slug": "como-agarrar-meu-ex-namorado"
+},
+];

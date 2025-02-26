597779d347571d8be14a99f87175b6e14978c0d3
telavip <megateca@outlook.com>
2025-02-26 07:54:24 -0300

    Update and rename 1623.js to 16233.js

---
+++ 16233.js
@@ -0,0 +1,47 @@
+window.seriesData = {
+    "950396": {
+        "id": "950396",
+        "name": "Entre Montanhas",
+        "slug": "entre-montanhas",
+        "overview": "Dois agentes altamente treinados se aproximam à distância após serem enviados para proteger lados opostos de um desfiladeiro misterioso. Quando um mal emerge, eles precisam trabalhar juntos para sobreviver ao que está lá dentro.",
+        "first_air_date": "2025-02-13",
+        "runtime": 127,
+        "episode_run_time": [
+            127
+        ],
+        "vote_average": "7.8",
+        "number_of_seasons": 1,
+        "number_of_episodes": 1,
+        "created_by": [
+            "Skydance Media",
+            "Crooked Highway",
+            "Apple Studios"
+        ],
+        "production_companies": [
+            {
+                "name": "Skydance Media"
+            },
+            {
+                "name": "Crooked Highway"
+            },
+            {
+                "name": "Apple Studios"
+            }
+        ],
+        "cast": [
+            "Miles Teller",
+            "Anya Taylor-Joy",
+            "Sigourney Weaver",
+            "Sope Dirisu",
+            "William Houston"
+        ],
+        "backdrop_path": "https://image.tmdb.org/t/p/original/9nhjGaFLKtddDPtPaX5EmKqsWdH.jpg",
+        "poster_path": "https://image.tmdb.org/t/p/original/7zL6RxMPmpPSdU5OTdVC4dtiKcr.jpg",
+        "certification": "14",
+        "genres": [
+            "Romance",
+            "Ficção científica",
+            "Thriller"
+        ]
+    }
+};
